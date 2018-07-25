ccd25611ced3f22e0e8d48426419351b3e6da68a
Chris Topher <programmingfire@gmail.com>
2018-07-25 13:42:02 -0500

    Add Del method and test with Display method

--- LinkedList/Ruby/linkedList.rb
+++ LinkedList/Ruby/linkedList.rb
@@ -22,19 +22,50 @@ class LinkedList
 		self
 	end
 
+	def del(val)
+		current = @head
+		if current.value == val
+			@head = @head.next_node
+		else
+			current = @head
+			while (current != nil && current.next_node != nil && ((current.next_node).value != val))
+			       current = current.next_node
+			end
+			if (current.value != nil) && (current.next_node != nil)
+				current.next_node = (current.next_node).next_node
+			end
+		end
+	end
+
+
 	def display
 		current = @head
 		full_list = []
 		while current != nil
-			puts current.value.to_s.to_s
+			full_list += [current.value.to_s]
 			current = current.next_node
 		end
+		full_list +=[current.to_s]
+		puts full_list.join("->")
 	end
 end
 
 
-
+#Test LinkedList class with Display Method
 ll = LinkedList.new(5)
 ll.add(7)
 ll.add(6)
+ll.add(8)
+ll.display
+ll.del(8)
+ll.display
+ll.del(7)
+ll.display
+ll.add(7)
+ll.display
+ll.del(99)
+ll.display
+ll.add(99)
+ll.display
+ll.del(99)
 ll.display
