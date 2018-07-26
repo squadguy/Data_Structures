bc8d27b9185ab5f776f0b78e6ea2bb737d87f2ad
Chris Topher <programmingfire@gmail.com>
2018-07-25 20:02:49 -0500

    Create linked list with methods

--- LinkedList/Ruby/deLinkedList.rb
+++ LinkedList/Ruby/deLinkedList.rb
@@ -1,10 +1,9 @@
-
 ##Code for a doubly Linked List
 ##Class node defines everything needed to interface with a node
 ##Class Linked List define everything needed to interface with linked list
 
 class Node
-	def initialize data, ptr_next = nil
+	def initialize data, ptr_next = nil 
 		@data = data
 		@ptr_next = ptr_next
 	end
@@ -23,11 +22,13 @@ class Node
 	def display
 		puts @data
 	end
+
 end
 
 class LL
 	def initialize data
 		@head = Node.new(data, nil)
+		@length = 1
 	end
 
 	def display
@@ -46,11 +47,64 @@ class LL
 			current = current.get_next
 		end
 		current.set_next(tmp)
+		@length += 1
+
+	end
+
+	def insert_at_front data
+		current = @head
+		tmp = Node.new(data, nil)
+		
+		tmp.set_next(@head)
+		@head = tmp
+
+		@length +=1
+	end
+
+	def insert_at index, data
+		current = @head
+		tmp = Node.new(data,nil)
+		iterator = 0
+	#check if it's inbounds
+		if(index <= @length)
+			if (index == 0)
+				insert_at_front(data)
+			elsif (index == @length)
+				insert_at_end(data)
+				puts "calling insert_at_end"
+			else
+				while (!current.next? && iterator < index)
+					current = current.get_next
+					iterator += 1
+				end
+				tmp.set_next(current.get_next)
+				current.set_next(tmp)
+
+				@length += 1	
+			end
+		else
+			puts "Index out of Range"	
+		end
+	end
+		
+
+	def get_length
+		return @length
 	end
 end
 
-ll = LL.new(5)
-ll.insert_at_end 2 
-ll.insert_at_end 15
-ll.insert_at_end 20 
+ll = LL.new(1)
 ll.display
+puts ll.get_length
+puts "\n"
+
+ll.insert_at(1,3)
+ll.display
+puts ll.get_length
+puts "\n"
+
+ll.insert_at(1,2)
+ll.display
+puts ll.get_length
+puts "\n"
+
