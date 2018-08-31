c1f768f58ae55b9ed48a82b45a60b86c856a99bd
Chris Topher <programmingfire@gmail.com>
2018-08-31 06:00:03 -0500

    Move Struct to Declaration area

--- Trees/C/tree.c
+++ Trees/C/tree.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+	struct node* left;
+	struct node* right;
+	int value;
+} node;
+
 int main()
 {
-	typedef struct node
-	{
-		struct node* left;
-		struct node* right;
-		int value;
-	} node;
 
 
 	node *root = malloc(sizeof(node));
