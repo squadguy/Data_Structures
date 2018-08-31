585b4e8b2a20092e76e969e9a1ac6add958f0473
Chris Topher <programmingfire@gmail.com>
2018-08-31 14:25:50 -0500

    Add addNode function

--- Trees/C/tree.c
+++ Trees/C/tree.c
@@ -11,20 +11,33 @@ typedef struct node
 node* createNode(int);
 void insertLeft(node*, int);
 void insertRight(node*, int);
+void addNode (node* root, int data);
 int main()
 {
 
 
 	node *root = malloc(sizeof(node));
 	root = createNode(8);	
+	/*
 	insertLeft(root, 4);
 	insertRight(root, 9);
 	insertLeft(root->left, 2);
+	*/
+
+	addNode(root,10);
+	addNode(root,7);
+	addNode(root,5);
+	addNode(root,12);
+
 
 	printf("%d\n", root->value);
-	printf("%d\n", root->left->value);
 	printf("%d\n", root->right->value);
+	printf("%d\n", root->left->value);
 	printf("%d\n", root->left->left->value);
+	printf("%d\n", root->right->right->value);
+
+//	printf("%d\n", root->right->value);
+//	printf("%d\n", root->left->left->value);
 
 	return 0;
 }
@@ -59,4 +72,38 @@ void insertRight (node* root, int data)
 	tmpNode->left = NULL;
 	tmpNode ->right = NULL;
 	root->right = tmpNode;
+}
+
+void addNode (node* root, int data)
+{
+	node* currentNode = root;
+	int bs = 0;
+	int* ptrBS = &bs;
+	if (currentNode == NULL){
+		root = malloc(sizeof(node));
+		root->left = NULL;
+		root->right = NULL;
+		root->value = data;
+	} else {
+	while (*ptrBS == 0)
+		if(currentNode->value >= data)
+		{
+			if(currentNode->left == NULL)
+			{
+				insertLeft(currentNode, data);
+				*ptrBS=1;
+			} else{
+			       	currentNode = currentNode->left;
+			}
+		} else {
+			if(currentNode->right == NULL)
+			{
+				insertRight(currentNode, data);
+				*ptrBS=1;
+			}else{
+				currentNode = currentNode->right;
+			}
+		}
+	}
+	
 }
