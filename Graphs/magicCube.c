2b52169bc07bbbe73ff9e52f35b828399a1f3020
Chris Topher <programmingfire@gmail.com>
2018-09-14 01:02:28 -0500

    Create Headers and Implemenation sections

--- Graphs/magicCube.c
+++ Graphs/magicCube.c
@@ -1,66 +1,69 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
-int main(){
+/*
+ *must compile with -lm after source file for math lib
+ */
+
 
 	//Define a node
 	typedef struct node{
 		int data;
-		struct node* up;
-		struct node* down;
-		struct node* right;
-		struct node* left;
-		struct node* diagUpLeft;
-		struct node* diagUpRight;
-		struct node* diagDownRight;
-		struct node* diagDownLeft;
+		struct node* next;
 	} node;
 
+//creates a node
+	node* createNode(int data);
+
+
+//Finds val that goes in middle 
+	int findMiddleVal(int *);
+
+int main(){
 
 	//number of node in a side
 	int sideSize = 3;
 	//create an array representing the magic Squar
-	node* magicSquare[sideSize][sideSize];
+	node *magicSquare[sideSize][sideSize];
+
+	//Our 'foothold' in [0][0] of our magic square
+	//ceil returns a double need to cast sideSize to Double and then Cast back to int
+	//int cornerCase = (int)ceil((double)sideSize/2);
 
 		
-//Create a magic cube node
-	node* createNode(int data){
-	
-		node* tmp = malloc(sizeof(node));
-
-		tmp->data = data;
-		tmp->up = NULL;
-		tmp->down = NULL;
-		tmp->right = NULL;
-		tmp->left = NULL;
-		tmp->diagUpLeft = NULL;
-		tmp->diagUpRight = NULL;
-		tmp->diagDownRight = NULL;
-		tmp->diagDownLeft = NULL;
-
-		return tmp;
-	}
+	int midVal = findMiddleVal(&sideSize);
 
 	
 //Init the magic square (need to make a function)
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < sideSize; ++i)
 	{
-		for (int j = 0; j < 3; ++j)
+		for (int j = 0; j < sideSize; ++j)
 		{
 			magicSquare[i][j] = createNode(0);
-			
 		}
 	}
-
-//test
-	magicSquare[0][2]->down = magicSquare[2][2];
-
-	magicSquare[2][2]->data = 100;
-
-
-
-	printf("%d\n", magicSquare[0][2]->down->data);
 
 
 	return 0;
 }
+
+//Find the value which will go in the middle of the matrix
+	int findMiddleVal(int* sideSize)
+	{
+
+		int midVal = *sideSize/2;
+
+		return midVal;
+	}	
+
+//Create a magic cube node
+	node* createNode(int data){
+		node* tmp = malloc(sizeof(node));
+
+		tmp->data = data;
+		node* next = NULL;
+		node* prev = NULL;
+
+		return tmp;
+	}
